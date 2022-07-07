264389465e02deeac96824e384e6e45a02ac008c
Westin <71640556+staf123@users.noreply.github.com>
2022-07-07 11:29:07 +0700

    Create config.py

---
+++ config.py
@@ -0,0 +1,6 @@
+CONFIG = {
+    'POSTGRES_URI': 'postgresql://postgres:@127.0.0.1:5431/flask_db',
+    'SQLALCHEMY_DATABASE_URI': 'postgresql://postgres:@127.0.0.1/flask_db',
+    'SQLALCHEMY_TRACK_MODIFICATIONS': False}
+
+SALT = 'my_sJHLHLHKLаваыпuper_s!alt_#4$4344'
